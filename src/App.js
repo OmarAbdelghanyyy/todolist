6171bca3382e1c41a58941a5e238d03583c7a9c0
Omar Abdelghany <oabdelghany1409@gmail.com>
2023-01-03 16:04:23 +0300

    modified:   src/App.css 	modified:   src/App.js 	new file:   src/ToDo.jsx 	new file:   src/images/addButton.svg 	new file:   src/images/removeButton.svg

--- src/App.js
+++ src/App.js
@@ -1,25 +1,68 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
+import addButton from './images/addButton.svg';
+import removeButton from'./images/removeButton.svg';
+import ToDo from './ToDo.jsx';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  const [todoList, setTodoList] = useState([]);
+  const [todo,setTodo] = useState('');
+  function addToDo(){
+    const newToDoList=[...todoList,todo];
+    setTodoList(newToDoList);
+    setTodo('');
+    console.log(todo);
+    console.log(todoList);
+  }
+  useEffect(()=>{
+    setTodo();
+   
+  },[])
+
+function removeToDo(){
+  // ideally add ticks next to all todos and then you select the ones you want to delete by ticking them
+}
+
+return(
+  <div className='App'>
+    <div className='title'>
+      <h1>TodoApp</h1>
     </div>
-  );
+    <div className='bar'>
+      <input
+      placeholder='Add a Todo'
+      value={todo}
+      onChange={(e)=>{
+        setTodo(e.target.value)
+      }} 
+      />
+
+      <img
+      className='add'
+      src={addButton}
+      alt='add'
+      onClick={()=>{
+        addToDo();
+      }}
+      />
+        <img
+      className='remove'
+      src={removeButton}
+      alt='remove'
+      onClick={()=>{
+        removeToDo()
+      }}
+      />
+    </div>
+    <div className='list'>
+      {todoList.map((todo)=>(
+        <ToDo todo={todo} />
+      ))}
+    </div>
+  </div>
+)
+
 }
 
 export default App;
---
+++ src/ToDo.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const ToDo= ({todo}) =>{
+    return(<div>
+        <input type="checkbox"/>{todo}
+    </div>
+    )
+}
+
+export default ToDo;
