742230bf39f61147b91ea11b246093a847ec817f
Omar Abdelghany <oabdelghany1409@gmail.com>
2023-01-04 17:15:36 +0300

    app functionnality completed

--- src/App.js
+++ src/App.js
@@ -1,28 +1,59 @@
 import logo from './logo.svg';
 import './App.css';
-import { useEffect, useState } from 'react';
+import { useEffect, useState,useRef } from 'react';
 import addButton from './images/addButton.svg';
 import removeButton from'./images/removeButton.svg';
-import ToDo from './ToDo.jsx';
+import ToDoList from './ToDoList';
+
+const LOCAL_STORAGE_KEY= 'savedList';
 
 function App() {
   const [todoList, setTodoList] = useState([]);
   const [todo,setTodo] = useState('');
-  function addToDo(){
-    const newToDoList=[...todoList,todo];
-    setTodoList(newToDoList);
-    setTodo('');
-    console.log(todo);
-    console.log(todoList);
-  }
   useEffect(()=>{
-    setTodo();
-   
+    const savedTodoList= JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY));
+    if(savedTodoList){
+      setTodoList(savedTodoList);
+    }
   },[])
+
+  useEffect(()=>{
+    if (todoList.length!==0){
+      localStorage.setItem(LOCAL_STORAGE_KEY,JSON.stringify(todoList));
+    }
+
+   
+  },[todoList])
+    
+  
+  function addToDo(){
+    if(todo==='') return
+    else{
+    setTodoList(prevtodo =>[...prevtodo,
+      {
+      id: todoList.length,
+      name : todo,
+      complete:false
+    }
+  ])  
+    setTodo('');
+}
+  }
+
+
 
 function removeToDo(){
   // ideally add ticks next to all todos and then you select the ones you want to delete by ticking them
+  const newTodos= todoList.filter(todo => !todo.complete) // returns array that only has the uncompleted ones
+  setTodoList(newTodos)
 }
+function toggleTodo(id) {
+  const newTodos = [...todoList]
+  const todo = newTodos.find(todo => todo.id === id)
+  todo.complete = !todo.complete
+  setTodoList(newTodos)
+}
+
 
 return(
   <div className='App'>
@@ -37,7 +68,6 @@ return(
         setTodo(e.target.value)
       }} 
       />
-
       <img
       className='add'
       src={addButton}
@@ -46,7 +76,7 @@ return(
         addToDo();
       }}
       />
-        <img
+      <img
       className='remove'
       src={removeButton}
       alt='remove'
@@ -56,13 +86,11 @@ return(
       />
     </div>
     <div className='list'>
-      {todoList.map((todo)=>(
-        <ToDo todo={todo} />
-      ))}
+    <ToDoList todos={todoList} toggleTodo={toggleTodo} />
     </div>
   </div>
 )
 
-}
+    }
 
 export default App;
--- src/ToDo.jsx
+++ src/ToDo.jsx
@@ -1,8 +1,19 @@
 import React from "react";
 
-const ToDo= ({todo}) =>{
-    return(<div>
-        <input type="checkbox"/>{todo}
+
+const ToDo= ({todo , toggleTodo}) =>{
+    function handleChange(){
+        toggleTodo(todo.id)
+    }
+    return(
+    <div className="todo">
+        <input 
+        type="checkbox" 
+        
+        checked={todo.complete}
+        onChange={handleChange}
+        />
+        <label htmlFor="checkbox">{todo.name}</label>
     </div>
     )
 }
